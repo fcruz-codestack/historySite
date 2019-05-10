cbf34672a04a7417975f3651f11939e3b1f71323
fcruz-codestack <fcruz@codestack.co>
2019-05-09 23:57:45 -0700

    disable lint rule

--- atlusSite/src/app/Components/welcome/welcome.component.ts
+++ atlusSite/src/app/Components/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+// tslint:disable-next-line: component-selector
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
